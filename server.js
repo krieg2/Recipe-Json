3580f02a3dc352b2745eb8a3bc4ec5d78ad784be
krieg2 <aaronk1@yahoo.com>
2018-01-05 20:47:35 -0500

    cors change

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-const routes = require("./routes/routes.js");
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+const routes = require("./routes/routes.js");
 app.use("/", routes);
 
 app.listen(PORT, () => {
